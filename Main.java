c542e5ef5976b5052ee8f2a8fc6b3d5c53d014a0
Elena Sidyakina <elenasidyakina@gmail.comm>
2017-01-31 11:27:58 -0800

    first

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Time obj1 = new Time();
+		obj1.setTime(13, 12, 58);
+		System.out.println("Military Time: "+ obj1.toMilitary());
+
+	}
+
+}
---
+++ Time.java
@@ -0,0 +1,21 @@
+
+public class Time {
+//create a Time class with three variables h,m,s
+	private int hour;
+	private int minute;
+	private int second;
+	
+//create a setTime method that validates h,m,s and sets time
+	
+	public void setTime(int h, int m, int s){
+		hour = ((h>=0 && h<24) ? h :0);
+		minute = ((m>=0 && m<60) ? m :0);
+		second = ((s>=0 && s<60) ? s :0);
+	}
+//create a method that prints out input as Military time
+	
+	public String toMilitary(){
+		return String.format("%02d:%02d:%02d", hour, minute, second);
+	}
+	
+}
